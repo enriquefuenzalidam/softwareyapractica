4bf3ac66d71c1137693bf79920e2629b8e722e10
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-10 09:13:27 -0400

    hj

--- app/page.jsx
+++ app/page.jsx
@@ -23,9 +23,10 @@ export default function Home() {
           <h2 data-aos-once="true" data-aos='fade-right' className={` mx-8 -translate-y-[0.31rem] sm:-translate-y-[0.31rem] md:-translate-y-[0.39rem] lg:-translate-y-[0.47rem] font-LexendDeca text-opacity-100 text-left w-full text-6xl md:text-7xl lg:text-8xl `} style={{ textShadow: `0 0 0.1rem #000, 0 0 0.6rem #000` }}><span className={`  font-thin text-[#B99CD0]`}>so</span><span className={`  font-extralight text-[#9F8CCC] `} >ft</span><span className={`  font-light text-[#867AC8] `} >wa</span><span className={`  font-semibold text-[#746FC6] `} >re</span><span className={`  font-extrabold  text-[#6364C3] `} >ya</span></h2>
         </div>
       </section>
-      <section className={` relative p-3 sm:p-4 md:p-5 lg:p-6  `}>
-        <div className={` absolute inset-0 bg-fucsiaAzulRatioFondo opacity-80 `}></div>
-        <div className={` absolute inset-0 bg-repeat `} style={{ backgroundImage: `url(images/noise.png)`, backgroundSize: `20%` }}></div>
+      
+      <section className={` relative p-3 sm:p-4 md:p-5 lg:p-6 bg-blue-900 `}>
+        <div className={` absolute inset-0 bg-fucsiaAzulRatioFondo opacity-60 `}></div>
+        <div className={` absolute inset-0 bg-repeat `} style={{ backgroundImage: `url(images/noise.png)`, backgroundSize: `15%` }}></div>
         <p className={` relative mx-auto max-w-5xl text-right pb-3 sm:pb-4 md:pb-5 lg:pb-6 `}>
           
           <span className={` overflow-hidden relative inline-block rounded-sm py-2 px-6  ${isEmpty ? `bg-sky-200` : `bg-[#faae3b]`} bg-opacity-70 shadow-inner shadow-neutral-700 uppercase font-medium font-Oswald text-black text-opacity-80 text-lg lg:text-xl `} >
@@ -33,7 +34,7 @@ export default function Home() {
           </span>
         </p>
         <div className={` relative rounded-sm mx-auto p-3 sm:p-4 md:p-6 lg:p-8 max-w-7xl bg-white bg-opacity-30  ck `}>
-          <h2 className={` mx-auto mb-8  max-w-7xl text-left text-xl sm:text-2xl md:text-3xl uppercase font-semibold  text-[#42347d] text-opacity-100 font-Oswald `}>Encuentra el software que buscas</h2>
+          <h2 className={` mx-auto mb-8  max-w-7xl text-left text-xl sm:text-2xl md:text-3xl uppercase font-medium  text-[#261b5b] text-opacity-100 font-Oswald `}>Encuentra el software que buscas</h2>
           <ProductList />
         </div>
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         extend: {
             backgroundImage: {
                 'cabeceraVideoFondo': 'linear-gradient(162deg,rgba(0,0,0,0.6) ,rgba(255,255,255,0), #636DC6)',
-                'fucsiaAzulRatioFondo': 'linear-gradient(162deg, #4351C6, rgba(211, 0, 126, 0.3), rgba(0, 0, 0,0.5), rgb(18, 33, 77), #000)',
+                'fucsiaAzulRatioFondo': 'linear-gradient(162deg, rgba(211, 0, 126, 0.3), rgba(0, 0, 0,0.5), #4351C6, rgb(18, 33, 77), #000)',
             },
             colors: {
                 neutral: colors.neutral
