93e4d57a6a9ff7653dc61a97f673eaf5c1a89f75
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-12 21:20:04 -0400

    ssdfsd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 
 import 'styles/globals.css';
 import { CartProvider } from './context/CartContext';
+import Footer from 'components/footer';
 
 export const metadata = {
   title: 'SoftwareYa'
@@ -10,11 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={` scroll-smooth `}>
       <body className={` relative bg-[#D4D4E1] min-w-full min-h-svh `}>
-        <div className={` absolute top-0 left-0 w-full h-full bg-metalicoGlassFondoB `}></div>
-        <div className={` absolute top-0 left-0 w-full h-full bg-repeat `} style={{ backgroundImage: `url(images/noise.png)` }}></div>
+        <div className={` absolute top-0 left-0 w-full h-full bg-metalicoGlassFondoB `} />
+        <div className={` absolute top-0 left-0 w-full h-full bg-repeat `} style={{ backgroundImage: `url(images/noise.png)` }} />
         <CartProvider>
           {children}
         </CartProvider>
+        <Footer />
       </body>
     </html>
   );
--- components/SoftwareCategory.jsx
+++ components/SoftwareCategory.jsx
@@ -24,8 +24,8 @@ const SoftwareCategory = () => {
 
   return (
     <div className={`relative rounded-sm mx-auto p-3 sm:p-4 md:p-6 lg:p-8 max-w-7xl bg-white bg-opacity-40`}>
-      <h2 className={`mx-auto mb-8 max-w-7xl text-left text-xl sm:text-2xl md:text-3xl uppercase text-violet-800 text-opacity-100 font-Oswald`}>
-        <span className={`font-light`}>Softwares dentro de categoría</span> <span className={`font-medium`}>{categoryName}</span>
+      <h2 className={`mx-auto mb-8 max-w-7xl text-left text-xl sm:text-2xl md:text-3xl uppercase text-opacity-100 font-Oswald`}>
+        <span className={`font-extralight text-[#261b5b]`}>Softwares en categoría</span> <span className={`font-medium text-[#2184b6] `}>{categoryName}</span>
       </h2>
       <CategoriaProductos categoryId={numericCatgoriaId} />
     </div>
