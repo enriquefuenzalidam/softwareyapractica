d10080813d04cb9e46eed3815b9b0b586162574d
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-16 16:57:42 -0400

    sdfsd

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,18 @@ import 'styles/globals.css';
 import { CartProvider } from './context/CartContext';
 import Footer from 'components/footer';
 
+
 export const metadata = {
-  title: 'SoftwareYa'
+  title: {
+      template: '%s | SoftwareYa',
+      default: 'SoftwareYa'
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={` scroll-smooth `}>
+      <head/>
       <body className={` relative bg-[#D4D4E1] min-w-full min-h-svh `}>
         <div className={` absolute top-0 left-0 w-full h-full bg-metalicoGlassFondoB `} />
         <div className={` absolute top-0 left-0 w-full h-full bg-repeat `} style={{ backgroundImage: `url(images/noise.png)` }} />
