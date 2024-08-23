c1c32c7639b63392f8eceb9b52ed72c4d7f7adbd
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-23 10:46:01 -0400

    sdsdf

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
-const { createServer } = require('http');
-const next = require('next');
+import { createServer } from 'http';
+import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
