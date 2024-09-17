897949d0567fc452a8342af8c26f767f7ad8f8a2
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-09-17 13:12:42 -0300

    sdfsdfs

--- components/fondoCabecera.jsx
+++ components/fondoCabecera.jsx
@@ -18,14 +18,14 @@ const FondoCabecera = ({ portada }) => {
       }, []);
  
     return (
-        <section className={` min-h-20 ${portada ? `h-[24vh] sm:h-[38vh] lg:h-[50vh] max-h-96 ` : ` h-[24vh] `}  relative w-full bg-cover bg-center bg-[#2d2820] overflow-hidden  `} id={`inicio`}>
-            <div className={` absolute top-0 left-0 w-full h-full opacity-30 `}>
+        <section className={` min-h-20 ${portada ? `h-[24vh] sm:h-[38vh] lg:h-[50vh] max-h-96 ` : ` h-[24vh] `}  relative w-full bg-cover bg-center bg-[#D9EFFC] overflow-hidden  `} id={`inicio`}>
+            <div className={` absolute top-0 left-0 w-full h-full opacity-50 `}>
                 <video className={` object-cover ${portada ? `object-left-top` : `object-center `} h-full w-full `} preload="auto" autoPlay="autoplay" muted loop playsInline>
                     <source src={portadaVideo} type="video/mp4" />
                 </video>
             </div>
             <div className={`  absolute top-0 left-0 w-full h-full bg-cabeceraVideoFondo `} />
-            <div className={`  absolute top-full left-0 w-full h-3 -translate-y-3 bg-gradient-to-t from-[rgba(0,0,0,0.2)] to-transparent `} />
+            <div className={`  absolute top-full left-0 w-full h-16 -translate-y-16 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-transparent `} />
             <div className={` relative mx-auto max-w-6xl flex w-full h-full items-end justify-start `}>
                 <h2 className={`  mx-8 -translate-y-[0rem] sm:-translate-y-[0.31rem] md:-translate-y-[0.39rem] lg:-translate-y-[0.47rem] font-LexendDeca text-opacity-100 text-left w-full text-4xl sm:text-5xl md:text-6xl lg:text-7xl `} style={{ textShadow: `0 0 0.3rem rgba(0,0,0,0.5), 0 0 0.6rem rgba(0,0,0,0.2), 0 0 1.5rem rgba(0,0,0,0.7)` }}>
                     {portada ?
